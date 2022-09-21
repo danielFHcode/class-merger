6bced78e932111f615a8877ad5cf708c5f46b95b
danfh <danfh9@gmail.com>
2022-09-21 20:25:29 +0300

    start-up

---
+++ classMerger.js
@@ -0,0 +1,17 @@
+/**
+ * @param {class[]} classList A list of classes to be merged.
+ * @returns {class} One merged class.
+ */
+ function mergeClasses(...classList){
+    if (classList.length == 1) return classList[0];
+
+    const currentClass = classList[0];
+    classList.splice(0,1);
+    const mergedClass = class extends mergeClasses(...classList){};
+
+    Object.getOwnPropertyNames(currentClass.prototype).forEach((name)=>{
+        mergedClass.prototype[name] = currentClass.prototype[name];
+    })
+
+    return mergedClass;
+}
